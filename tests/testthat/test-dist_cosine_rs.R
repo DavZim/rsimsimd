e1e04f255d55c80bba44a9ae686a4655c43ac941
David Zimmermann-Kollenda <david-zimmermann@hotmail.de>
2025-02-28 14:58:24 +0100

    update tests with reference implementation

--- tests/testthat/test-dist_cosine_rs.R
+++ tests/testthat/test-dist_cosine_rs.R
@@ -1,8 +1,41 @@
+# reference implementation in pure R
+ref_dist_cosine <- function(x, y) {
+  as.vector(crossprod(x, y) / sqrt(crossprod(x) * crossprod(y)))
+}
+ref_dist_cosine_list <- function(x, ll) sapply(ll, ref_dist_cosine, x = x)
+ref_dist_cosine_list_list <- function(list1, list2) {
+  do.call(
+    rbind,
+    lapply(
+      list1,
+      function(l1) {
+        sapply(
+          list2,
+          ref_dist_cosine,
+          x = l1
+        )
+      }
+    )
+  )
+}
+ref_dist_cosine_mat <- function(ll) {
+  mat <- matrix(1, nrow = length(ll), ncol = length(ll))
+  for (i in seq(length(ll))) {
+    for (j in seq(length(ll))) {
+      v <- ref_dist_cosine(ll[[i]], ll[[j]])
+      mat[i, j] <- v
+      mat[j, i] <- v
+    }
+  }
+  mat
+}
+
+
 test_that("Internal Rust functions work", {
   # test against hand-calculated result
   expect_equal(
     rsimsimd:::dist_cosine_rs(c(1, 2, 3), c(4, 5, 6)),
-    0.97463173,
+    ref_dist_cosine(c(1, 2, 3), c(4, 5, 6)),
     tolerance = 1e-6
   )
 
@@ -13,7 +46,7 @@ test_that("Internal Rust functions work", {
 
   # 1-1 relationship
   res <- rsimsimd:::dist_cosine_rs(vec1, vec2)
-  expect_equal(res, -0.010641766, tolerance = 1e-6)
+  expect_equal(res, ref_dist_cosine(vec1, vec2), tolerance = 1e-6)
 
 
   # one matrix -> all combinations
@@ -25,11 +58,7 @@ test_that("Internal Rust functions work", {
   expect_equal(res[lower.tri(res)], res[upper.tri(res)])
   expect_equal(res[diag(res)], c(1, 1, 1))
 
-  exp <- matrix(c(
-    1, 0.018478748, -0.046885613,
-    0.018478748, 1, -0.050507819,
-    -0.046885613, -0.050507819, 1
-  ), nrow = 3, byrow = TRUE)
+  exp <- ref_dist_cosine_mat(list1)
   expect_equal(res, exp, tolerance = 1e-6)
 
 
@@ -39,7 +68,7 @@ test_that("Internal Rust functions work", {
   res <- rsimsimd:::dist_cosine_single_mult_rs(vec1, list1)
   expect_equal(length(res), length(list1))
 
-  exp <- c(0.022898323, -0.003896248, 0.046008395)
+  exp <- ref_dist_cosine_list(vec1, list1)
   expect_equal(res, exp, tolerance = 1e-6)
 
 
@@ -51,10 +80,6 @@ test_that("Internal Rust functions work", {
   res <- rsimsimd:::dist_cosine_mult_mult_rs(list1, list2)
   expect_equal(dim(res), c(3, 3))
 
-  exp <- matrix(c(
-    -0.039135036, -0.0194967637, -0.0136295175,
-    0.0003882632, 0.0167732251, -0.0112813098,
-    -0.0125098021, 0.0198462835, -0.0006956676
-  ), nrow = 3, byrow = TRUE)
+  exp <- ref_dist_cosine_list_list(list1, list2)
   expect_equal(res, exp, tolerance = 1e-6)
 })
